7c4eb3a32ab654293ba02e0566a76723bb5f2556
ThomasLiCanada <147568115+ThomasLiCanada@users.noreply.github.com>
2023-10-11 09:16:39 -0400

    Add files via upload

---
+++ a02_account/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from a02_account.forms import RegistrationForm, AccountAuthenticationForm, AccountUpdateForm
+
+
+def registration_view(request):
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            return redirect('home')
+        else:
+            context['registration_form'] = form
+    else:  # get request
+        form = RegistrationForm
+        context['registration_form'] = form
+    return render(request, 'a02_account/register.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+# login
+def login_view(request):
+    context = {}
+
+    user = request.user
+    if user.is_authenticated:
+        return redirect("home")
+
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("home")
+    else:
+        form = AccountAuthenticationForm()
+
+    context['login_form'] = form
+    return render(request, 'a02_account/login.html', context)
+
+
+# account view
+def account_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+
+    context = {}
+    if request.POST:
+        form = AccountUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.initial = {
+                "email": request.POST['email'],
+                "username": request.POST['username'],
+            }
+            form.save()
+            context['account_form'] = form
+    else:
+        form = AccountUpdateForm(
+            initial={
+                "email": request.user.email,
+                "username": request.user.username,
+            })
+        context['account_form'] = form
+    return render(request, 'a02_account/account.html', context)
